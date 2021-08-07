5073cff76579eeff59ed4be30af611a018297ec2
JesusGuerrero <rocha.jesus@hotmail.com>
2021-08-07 00:41:54 -0700

    save html

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const Path = require('path');
 
 const init = async () => {
 
+    console.log(Path.join(__dirname, ''));
     const server = Hapi.server({
-        port: process.env.PORT || 4000
+        port: process.env.PORT || 4000,
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname)
+            }
+        }
     });
+
+    await server.register(require('@hapi/inert'));
+
 
     server.route({
         method: 'GET',
         path: '/',
         handler: (request, h) => {
 
-            return 'Hello World!';
+            return h.file('view.html')
         }
     });
 
